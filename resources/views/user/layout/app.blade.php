fc24f703fdce670be6cddc503624ad18f7d51b2c
Ahmet Arşiv <65822345+Arsivpro@users.noreply.github.com>
2021-08-06 18:51:35 +0300

    auth pages edited, auth lang tr added

--- resources/views/user/layout/app.blade.php
+++ resources/views/user/layout/app.blade.php
@@ -65,7 +65,7 @@
                     <i class="fa fa-bell fa-2x"></i><br>
                     Bildirimler
                 </a>
-                <a class="list-group-item list-group-item-action text-center mb-5" href="#">
+                <a class="list-group-item list-group-item-action text-center mb-5" href="{{route('logout')}}">
                     <i class="fas fa-sign-out-alt fa-2x"></i><br>
                     Çıkış Yap
                 </a>
@@ -92,7 +92,7 @@
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{route('user.profile')}}">Profil</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="">Çıkış Yap</a>
+                                <a class="dropdown-item" href="{{route('logout')}}">Çıkış Yap</a>
                             </div>
                         </li>
                     </ul>

bac39bb7ddb4ef20d211d52e6362e576c713ce50
Ali Karabay <alikarabay77@gmail.com>
2021-09-24 21:16:59 +0300

    Auth ui changed.

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -25,4 +25,35 @@
         </div>
     </div>
 </div>
+
+    @csrf
+    <div class="signin" [hidden]="login">
+        <h1 class="topline">Eposta adresinizi doğrulayın</h1>
+        <br />
+        @if (session('resent'))
+            <div class="alert alert-success" role="alert">
+                {{ __('E-posta adresinize yeni bir doğrulama bağlantısı gönderildi.') }}
+            </div>
+        @endif
+
+        <div class="input-field">
+            <input id="password" type="password" placeholder="Şifre" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+            @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+        <label>Devam etmeden önce, lütfen bir doğrulama bağlantısı için e-postanızı kontrol edin.</label>
+        <label>E-postayı almadıysanız</label>
+        <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+            @csrf
+            <div class="login-box-button">
+                <button type="submit">
+                    Başka bir istekte bulunmak için buraya tıklayın
+                </button>
+            </div>
+        </form>
+
+    </div>
 @endsection

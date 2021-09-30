4d2d3d995c9f34aa04ec66e5f8bcc453bb90d512
Ali Karabay <alikarabay77@gmail.com>
2021-09-30 18:33:18 +0300

    Datatable optimizeted.

--- resources/views/layouts/stylesheet.blade.php
+++ resources/views/layouts/stylesheet.blade.php
@@ -1,3 +1,2 @@
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.2.8/css/rowReorder.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.11.3/r-2.2.9/sb-1.2.2/sp-1.4.0/datatables.min.css"/>
+

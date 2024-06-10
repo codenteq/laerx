fb352dd64b0499cefc843f34720f6fa4577eeed3
Ahmet Sefa Arşiv <ahmetarsivpm@gmail.com>
2024-06-10 13:58:18 +0300

    Docs configure

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -9,10 +9,8 @@ class PaymentMethodSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
         DB::table('payment_methods')->insert([
             [
